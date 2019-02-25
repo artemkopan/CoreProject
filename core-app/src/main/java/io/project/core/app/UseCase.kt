45f2fd518da646651084ce7d4e4e65f4237ab3ed
Artem Kopan <artemkopan@gmail.com>
2019-02-25 08:04:32 +0200

    added async usecase

--- core-app/src/main/java/io/project/core/app/UseCase.kt
+++ core-app/src/main/java/io/project/core/app/UseCase.kt
@@ -12,3 +12,7 @@ interface UseCaseBiParams<Params1, Params2, Result> {
 interface UseCaseParams3<Params1, Params2, Params3, Result> {
     fun execute(params1: Params1, params2: Params2, params3: Params3): Result
 }
+
+interface UseCaseAsync<Params, Result> {
+    suspend fun execute(params: Params): Result
+}
