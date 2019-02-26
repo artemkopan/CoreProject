b86cfd0f05b3ef1dcd529e52d96c23da6a84c997
Artem Kopan <artemkopan@gmail.com>
2019-02-26 10:18:24 +0200

    added empty ctivity lifecycle

---
+++ core-android/src/main/java/io/project/core/android/EmptyActivityLifecycleCallbacks.kt
@@ -0,0 +1,37 @@
+package io.project.core.android
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+abstract class EmptyActivityLifecycleCallbacks : Application.ActivityLifecycleCallbacks {
+
+    override fun onActivityPaused(activity: Activity) {
+        //no-op
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+        //no-op
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+        //no-op
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+        //no-op
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle?) {
+        //no-op
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+        //no-op
+    }
+
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+        //no-op
+    }
+
+}
