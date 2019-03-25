8bcd12169a60f3e655d316622af7034d26272d10
Artem Kopan <artemkopan@gmail.com>
2019-03-25 07:59:08 +0200

    added UseCase without params;

--- core-app/src/main/java/io/project/core/app/UseCase.kt
+++ core-app/src/main/java/io/project/core/app/UseCase.kt
@@ -1,7 +1,11 @@
 package io.project.core.app
 
 
-interface UseCase<Params, Result> {
+interface UseCase<Result> {
+    fun execute(): Result
+}
+
+interface UseCaseParams<Params, Result> {
     fun execute(params: Params): Result
 }
 
@@ -13,7 +17,11 @@ interface UseCaseParams3<Params1, Params2, Params3, Result> {
     fun execute(params1: Params1, params2: Params2, params3: Params3): Result
 }
 
-interface UseCaseAsync<Params, Result> {
+interface UseCaseAsync<Result> {
+    suspend fun execute(): Result
+}
+
+interface UseCaseAsyncParams<Params, Result> {
     suspend fun execute(params: Params): Result
 }
 
