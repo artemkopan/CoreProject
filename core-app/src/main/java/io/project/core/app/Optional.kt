41d16539cdd61f1b6f1554b74b13fe50d6e0576c
Artem Kopan <artemkopan.dev@gmail.com>
2019-03-28 10:57:07 +0200

    added map methods to Optional

--- core-app/src/main/java/io/project/core/app/Optional.kt
+++ core-app/src/main/java/io/project/core/app/Optional.kt
@@ -8,7 +8,11 @@ class Optional<T> private constructor(val data: T?) : Serializable {
     fun isNotEmpty() = !isEmpty()
 
     fun get(default: () -> T) = data ?: default()
+    fun <M> getAndMap(transform: (T) -> M, default: () -> M) = data?.let(transform) ?: default()
     suspend fun getAsync(default: suspend () -> T) = data ?: default()
+    suspend fun <M> getAndMapAsync(transform: suspend (T) -> M, default: suspend () -> M): M {
+        return data?.let { transform(it) } ?: default()
+    }
 
     fun getOrThrow(message: () -> String = { "Data is null" }) = data ?: throw NullPointerException(message())
 
