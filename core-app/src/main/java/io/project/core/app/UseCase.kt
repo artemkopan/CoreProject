014089538040603d181ff4d400db0249bda3ebcf
Artem Kopan <artemkopan@gmail.com>
2019-04-01 18:02:30 +0300

    added new async use case with 3 params;

--- core-app/src/main/java/io/project/core/app/UseCase.kt
+++ core-app/src/main/java/io/project/core/app/UseCase.kt
@@ -28,3 +28,7 @@ interface UseCaseAsyncParams<Params, Result> {
 interface UseCaseAsyncBiParams<Params1, Params2, Result> {
     suspend fun execute(params1: Params1, params2: Params2): Result
 }
+
+interface UseCaseAsyncParams3<Params1, Params2, Params3, Result> {
+    suspend fun execute(params1: Params1, params2: Params2, params3: Params3): Result
+}
