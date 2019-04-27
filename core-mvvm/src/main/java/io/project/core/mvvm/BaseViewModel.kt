2c0480ba8ef9e9412e6944542dbfe520c018a5b9
kopanartem <274733831536>
2019-04-27 17:40:43 +0300

    removed unused live data in BaseViewModel

--- core-mvvm/src/main/java/io/project/core/mvvm/BaseViewModel.kt
+++ core-mvvm/src/main/java/io/project/core/mvvm/BaseViewModel.kt
@@ -1,19 +1,9 @@
 package io.project.core.mvvm
 
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModel : ViewModel(), BaseViewModelDelegate, LifecycleOwner {
+abstract class BaseViewModel : ViewModel(), BaseViewModelDelegate {
 
-    @Suppress("LeakingThis") //LifecycleRegistry wraps "this" to WeakReference
-    private val lifecycleRegistry = LifecycleRegistry(this)
-
-    override fun onCleared() {
-        lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
-    }
-
-    override fun getLifecycle(): Lifecycle = lifecycleRegistry
+    abstract override fun onCleared()
 
 }
