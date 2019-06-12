a856d13d460ebf742207032000a3203f46d6c728
kopanartem <274733831536>
2019-06-12 10:34:03 +0300

    removed deprecated coroutine extensions;

--- core-coroutines/src/main/java/io/project/core/coroutines/CoroutineExtensions.kt
+++ core-coroutines/src/main/java/io/project/core/coroutines/CoroutineExtensions.kt
@@ -44,7 +44,6 @@ suspend fun <T> ReceiveChannel<T>.subscribe(job: Job, onNext: suspend ReceiveCha
     }
 }
 
-
 suspend inline fun <T> ReceiveChannel<T>.startWith(item: T, context: CoroutineContext = Dispatchers.Unconfined): ReceiveChannel<T> =
     GlobalScope.produce(context = context, onCompletion = this.consumes()) {
         send(item)
@@ -53,18 +52,6 @@ suspend inline fun <T> ReceiveChannel<T>.startWith(item: T, context: CoroutineCo
         }
     }
 
-suspend inline fun <T> ReceiveChannel<T>.distinctUntilChanged(context: CoroutineContext = Dispatchers.Unconfined): ReceiveChannel<T> =
-    GlobalScope.produce(context, onCompletion = consumes()) {
-        var prev: T? = null
-        for (current in this@distinctUntilChanged) {
-            if (current != prev) {
-                send(current)
-                prev = current
-            }
-        }
-    }
-
-
 suspend inline fun <T> ReceiveChannel<T>.onNext(
     context: CoroutineContext = Dispatchers.Unconfined,
     crossinline onNext: suspend ReceiveChannel<T>.(T) -> Unit
@@ -72,57 +59,3 @@ suspend inline fun <T> ReceiveChannel<T>.onNext(
     GlobalScope.produce(context, onCompletion = consumes()) {
         consumeEach { onNext(it) }
     }
-
-
-suspend inline fun <T> ReceiveChannel<T>.distinctUntilChanged(
-    context: CoroutineContext = Dispatchers.Unconfined,
-    crossinline comparator: (T, T) -> Boolean
-): ReceiveChannel<T> =
-    GlobalScope.produce(context, onCompletion = consumes()) {
-        var prev: T = receive()
-        send(prev)
-        consumeEach {
-            if (!comparator(it, prev)) {
-                send(it)
-                prev = it
-            }
-        }
-    }
-
-/**
- * @function - zip: Combine items from two Deferreds together via a specified function and return items based on the results of this function
- * @param source1 - first deferred, whose result will be used in zipper
- * @param source2 - second deferred, whose result will be used in zipper
- * @param zipper - function, that will be called with deferred's results
- */
-suspend inline fun <T1, T2, R> zip(source1: Deferred<T1>, source2: Deferred<T2>, coroutineStart: CoroutineStart = CoroutineStart.DEFAULT, crossinline zipper: (T1, T2) -> R): Deferred<R> =
-    coroutineScope {
-        async(start = coroutineStart) {
-            zipper(source1.await(), source2.await())
-        }
-    }
-
-inline fun <T> ReceiveChannel<T>.debounce(
-    wait: Long = 300,
-    context: CoroutineContext = Dispatchers.Unconfined
-): ReceiveChannel<T> =
-    GlobalScope.produce(context, onCompletion = consumes()) {
-    var nextTime = 0L
-    consumeEach {
-        val curTime = System.currentTimeMillis()
-        if (curTime < nextTime) {
-            // not enough time passed from last send
-            delay(nextTime - curTime)
-            var mostRecent = it
-            while (!isEmpty) {
-                mostRecent = receive()
-            } // take the most recently sent without waiting
-            nextTime += wait // maintain strict time interval between sends
-            send(mostRecent)
-        } else {
-            // big pause between original events
-            nextTime = curTime + wait // start tracking time interval from scratch
-            send(it)
-        }
-    }
-}
