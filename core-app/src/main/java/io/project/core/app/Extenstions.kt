91d8368f1afb34aa87fda7b9290c3f3f02178a5a
kopanartem <274733831536>
2019-09-25 08:40:55 +0300

    added new extensions functions: - CharSequence? to Double or zero - CharSequence? to Int or zero

--- core-app/src/main/java/io/project/core/app/Extenstions.kt
+++ core-app/src/main/java/io/project/core/app/Extenstions.kt
@@ -34,6 +34,9 @@ inline fun Double?.orZero() = this ?: 0.0
 inline fun Float?.orZero() = this ?: 0f
 inline fun Int?.orZero() = this ?: 0
 
+inline fun CharSequence?.toDoubleOrZero() = toStringOrEmpty().toDoubleOrNull().orZero()
+inline fun CharSequence?.toIntOrZero() = toStringOrEmpty().toIntOrNull().orZero()
+
 inline fun Int.convertColorToHex() = String.format("#%06X", 0xFFFFFF and this)
 
 fun lerp(from: Float, to: Float, @FloatRange(from = 0.0, to = 1.0) fraction: Float): Float {
