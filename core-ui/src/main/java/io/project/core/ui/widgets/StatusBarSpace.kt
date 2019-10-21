e26ea319662fb80ae23dc16a347c9564c0bbe427
kopanartem <274733831536>
2019-10-21 17:24:18 +0300

    fixed status bar views;

--- core-ui/src/main/java/io/project/core/ui/widgets/StatusBarSpace.kt
+++ core-ui/src/main/java/io/project/core/ui/widgets/StatusBarSpace.kt
@@ -3,6 +3,7 @@ package io.project.core.ui.widgets
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import io.project.core.android.extensions.getStatusBarHeight
@@ -14,7 +15,11 @@ class StatusBarSpace @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), context.getStatusBarHeight())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), context.getStatusBarHeight())
+        } else {
+            setMeasuredDimension(0, 0)
+        }
     }
 
     @SuppressLint("MissingSuperCall")
--- core-ui/src/main/java/io/project/core/ui/widgets/StatusBarView.kt
+++ core-ui/src/main/java/io/project/core/ui/widgets/StatusBarView.kt
@@ -1,6 +1,7 @@
 package io.project.core.ui.widgets
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 import io.project.core.android.extensions.getStatusBarHeight
@@ -12,7 +13,11 @@ class StatusBarView @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), context.getStatusBarHeight())
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), context.getStatusBarHeight())
+        } else {
+            setMeasuredDimension(0, 0)
+        }
     }
 
 }
